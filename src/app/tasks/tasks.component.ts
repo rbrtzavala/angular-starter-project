822fc6bcd20899501824ec86b796b641ad9447f5
Robert Zavala <rbrtzavala@gmail.com>
2024-06-04 15:56:35 -0600

    create task component and filter selected tasks by user id

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,11 +1,27 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { TaskComponent } from "./task/task.component";
 
 @Component({
-  selector: 'app-tasks',
-  standalone: true,
-  templateUrl: './tasks.component.html',
-  styleUrl: './tasks.component.css'
+    selector: 'app-tasks',
+    standalone: true,
+    templateUrl: './tasks.component.html',
+    styleUrl: './tasks.component.css',
+    imports: [TaskComponent]
 })
 export class TasksComponent {
+  @Input({ required: true }) userId!: string;
   @Input({required: true}) name!: string;
+  tasks = [
+    {
+      id: 't1',
+      userId : 'u1',
+      title: 'Master Angular',
+      summary: 'Learn basic & advanced features of Angular and how to apply them.',
+      dueDate: '2025-12-31'
+    }
+  ];
+
+  get selectedUserTasks() {
+    return this.tasks.filter(task => task.userId === this.userId)
+  }
 }
