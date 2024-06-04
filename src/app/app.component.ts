3744eeed2c803be74298de7ce8e15405e0b952d0
Robert Zavala <rbrtzavala@gmail.com>
2024-06-04 11:47:21 -0600

    move dummy user data to top app level and refacto user component to use signals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
+import { DUMMY_USERS } from './dummy-users';
 
 
 @Component({
@@ -14,4 +15,6 @@ import { UserComponent } from "./user/user.component";
         UserComponent
     ]
 })
-export class AppComponent {}
+export class AppComponent {
+    users = DUMMY_USERS;
+}
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,10 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
-
-import { DUMMY_USERS } from '../dummy-users';
-
-const randomIndex = () => {
-  return Math.floor(Math.random() * DUMMY_USERS.length);
-}
+import { Component, Input, input, computed } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -12,21 +6,20 @@ const randomIndex = () => {
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
-export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex()])
-  // get image path using a computed method with signals
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
-  
-  // get imagePath() {
-  //   return 'assets/users/' + this.selectedUser.avatar;
-  // }
-  
-  onSelectUser() {
-    // 1. Option for managing state
-    // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    // this.selectedUser = DUMMY_USERS[randomIndex()];
 
-    // 2. Manage state with signal()
-    this.selectedUser.set(DUMMY_USERS[randomIndex()])
-  }
+export class UserComponent {
+  // @Input({required: true}) avatar!: string;
+  // @Input({required: true}) name!: string;
+  avatar = input.required<string>();
+  name = input.required<string>();
+
+  imagePath = computed(() => {
+    return 'assets/users/' + this.avatar()
+  });
+
+  // get imagePath() {
+  //   return 'assets/users/' + this.avatar
+  // }
+
+  onSelectUser() {}
 }
